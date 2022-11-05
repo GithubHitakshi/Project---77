ae5166b80636103e45db4d25abb0e320f8c712c5
GithubHitakshi <100277458+GithubHitakshi@users.noreply.github.com>
2022-11-05 17:42:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var first_Name = ("Hitakshi");
+console.log (first_Name);
+
+var last_Name = "Mittal";
+console.log (last_Name);
+
+var full_name=first_Name.concat(last_Name);
+console.log (full_name);
+
+var length_of_full_name=full_name.length;
+console.log(length_of_full_name);
